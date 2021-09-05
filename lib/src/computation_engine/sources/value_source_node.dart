13e066a33563bf5fdccd323dfb52ae983a06a37a
Andre Cruz <avacpim@gmail.com>
2021-09-05 14:28:48 +0200

    Make NodeValue internal value public

--- lib/src/computation_engine/sources/value_source_node.dart
+++ lib/src/computation_engine/sources/value_source_node.dart
@@ -2,15 +2,20 @@ import '../../../computation_engine.dart';
 
 /// A [SourceNode] that stores a value of type T
 class ValueSourceNode<T> extends SourceNode<T> {
-  final T _value;
+  final T value;
 
   ValueSourceNode(
     /// The [value] to be stored in this [Node]
-    T value,
-  ) : _value = value;
+    this.value,
+  );
 
   @override
   NodeValue<T> recompute() {
-    return NodeValue(_value);
+    return NodeValue(value);
+  }
+
+  @override
+  String toString() {
+    return value.toString();
   }
 }
